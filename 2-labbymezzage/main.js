caa5a07855feef6c7d445a21caba539d5fa03fb8
kl222kc <pi@raspberrypi.(none)>
2013-12-06 01:30:34 +0100

    Moment 2 – Message-objektet

--- 2-labbymezzage/main.js
+++ 2-labbymezzage/main.js
@@ -4,8 +4,16 @@ var main = {
 
 init: window.onload = function() {
 
-alert("test");
-
+var mess = new Message("Testmeddelande", new Date());
+alert(mess);
+alert(mess.getText());
+alert(mess.getDate());
+mess.setText("En annan\n text");
+alert(mess);
+mess.setDate(1989);
+alert(mess.getDate());
+alert(mess.getHTMLtext());
+alert(mess);
 }
 
 };
--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -1,5 +1,31 @@
 "use strict";
 
-function Message() {
+function Message(message, date) {
 	
+	this.getText = function() {
+		return message;
+	}
+
+	this.setText = function(_message) {
+		message = _message;
+	}
+
+	this.getDate = function() {
+		return date;
+	}
+
+	this.setDate = function(_date) {
+		date = _date;
+	}
+
+	Message.prototype.toString = function() {
+		return this.getText()+" ("+this.getDate()+")";
+	}
+
+	Message.prototype.getHTMLtext = function() {
+
+		var str = this.getText();
+		return str.replace(/\n/g, '<br />');
+	}	
 }
+
