f8360f2702a2737822ee083c76cb5165f7ee6ed0
kl222kc <pi@raspberrypi.(none)>
2013-12-10 23:00:00 +0100

    funktion skapad för att skriva ut spelplanen

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,11 +2,31 @@
 
 var Memory = {
 
-	memory: [],
+	board: [],
 
 	init: window.onload = function() {
-		var random;
-		random = new RandomGenerator.getPictureArray(4, 4);
-		console.log(random);
+		var rows = 4;
+		var cols = 4;
+		Memory.createBoard(rows, cols);
+	},
+
+	// Funktionen som skapar spelplanen
+	createBoard: function(rows, cols) {
+
+	// Generar spelplanen
+	Memory.board = new RandomGenerator.getPictureArray(rows, cols);
+
+	// Skapar och skriver ut spelplanen
+	for(var i=0; i < Memory.board.length; ++i) {
+
+		var div = document.querySelector("#board");
+		var number = document.createElement("img");
+
+		number.setAttribute("src","pics/0.png");
+
+ 		number.innerHTML += Memory.board[i];
+ 		div.appendChild(number);
+ 	}
+	
 	},
 };
