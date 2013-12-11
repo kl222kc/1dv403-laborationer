1735ed23ba16da06a968eceed7d9682ba8c115bd
kl222kc <pi@raspberrypi.(none)>
2013-12-11 03:45:59 +0100

    metod för att vända korten

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -13,24 +13,43 @@ var Memory = {
 	// Funktionen som skapar spelplanen
 	createBoard: function(rows, cols) {
 
-	// Generar spelplanen
+	// Generar arrayen som spelplanen bygger på
 	Memory.board = new RandomGenerator.getPictureArray(rows, cols);
 
 	// Skapar och skriver ut spelplanen
 	for(var i=0; i < Memory.board.length; ++i) {
 
 		var div = document.querySelector("#board");
+
 		var card = document.createElement("img");
 		card.setAttribute("src","pics/0.png");
-		card.setAttribute("class","card");
 
+		var link = document.createElement("a");
+		link.setAttribute("href", "#");
+		link.setAttribute("class", "card");
+		link.setAttribute("id", Memory.board[i]);
+
+		// Skapar en ny rad
 		if (i % cols === 0) {
-			card.setAttribute("class","card newrow");
+			link.setAttribute("class","card newrow");
 		}
 
- 		card.innerHTML += Memory.board[i];
- 		div.appendChild(card);
- 	}
+		link.appendChild(card);
+		div.appendChild(link);
+
+ 		// Kollar ifall man klickar på något kort
+ 		link.onclick = function(e) {
+			e.preventDefault(); // prevents the default action the browser makes on that event.
+			Memory.pickCard(this);
+		};
+
+	}
+
+},
+
+	// Funktion som visar korten
+	pickCard: function(link) {
+		link.querySelector('a > img').setAttribute("src","pics/" + link.id +".png");
+	}
 	
-	},
 };
