ec61c604b3071dc74447b8e1498ba4a2ee919263
kl222kc <pi@raspberrypi.(none)>
2014-01-06 06:21:46 +0100

    Fönster skapat och går att stänga

--- 5-pwd/js/main.js
+++ 5-pwd/js/main.js
@@ -7,11 +7,37 @@ var main = {
 	{
 
 		main.icon.onclick = function(){
+
+			if (!document.getElementById("window")) {
+				main.createWindow();
+			}
 			
-			console.log("test");
 		}
 
+	},
+
+	createWindow: function() {
+
+		var body = document.querySelector("body");
+		var window = document.createElement("div");
+		var closeButton = document.createElement("img");
+
+	 	closeButton.setAttribute("src","pics/close.png");
+ 		closeButton.setAttribute("class","closeButton");
+
+ 		closeButton.addEventListener("click", closeWindow, false)
+
+ 		function closeWindow() {
+ 			document.body.removeChild(window);
+ 		}
+
+
+		window.setAttribute("id","window"); 
+		window.appendChild(closeButton);
+ 		body.appendChild(window);
+		
 	}
+
 };
 
 window.onload = main.init;
